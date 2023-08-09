f2ca5cc083809887c6827e38e1e405cc20bb8268
wesleyfuchter <wesley.fuchter@gmail.com>
2023-08-09 19:17:40 -0300

    init

---
+++ test/cdk-people-list.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkPeopleList from '../lib/cdk-people-list-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-people-list-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkPeopleList.CdkPeopleListStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
