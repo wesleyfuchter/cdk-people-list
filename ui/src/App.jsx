fb82205d413d95db950811bc6aaf789ee5da3e38
wesleyfuchter <wesley.fuchter@gmail.com>
2023-08-11 20:18:46 -0300

    test pipeline #7

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprenda React
         </a>
       </header>
     </div>
