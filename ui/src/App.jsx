461b8ca11ed9c6a36c30da8b1e37a1782f2a75e6
wesleyfuchter <wesley.fuchter@gmail.com>
2023-08-11 20:15:24 -0300

    test pipeline #3

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
           {' '}
           <code>src/App.js</code>
           {' '}
-          and save to reload. Vasco
+          and save to reload.
         </p>
         <a
           className="App-link"
