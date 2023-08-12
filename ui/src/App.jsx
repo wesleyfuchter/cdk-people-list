633ff54186b09b86580da102c85f26146e32faa6
wesleyfuchter <wesley.fuchter@gmail.com>
2023-08-11 21:15:22 -0300

    test pipeline

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -4,7 +4,6 @@ import logo from './logo.svg';
 import './App.css';
 
 function App({ lang }) {
-  console.log('lang', lang);
   return (
     <div className="App">
       <header className="App-header">
--- ui/src/App.test.jsx
+++ ui/src/App.test.jsx
@@ -3,13 +3,13 @@ import { render, screen } from '@testing-library/react';
 import { test, expect } from '@jest/globals';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders learn react link in portuguese', () => {
   render(<App lang="BR" />);
   const linkElement = screen.getByText(/aprenda react/i);
   expect(linkElement).toBeInTheDocument();
 });
 
-test('renders learn react link', () => {
+test('renders learn react link in english', () => {
   render(<App lang="EN" />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
